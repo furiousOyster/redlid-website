aa1928df84671c4ad4e3b9f8a065827fb9e92f9f
Peter Casale <casalepeter@gmail.com>
2020-03-11 14:26:27 +1300

    Zone selector created for skips page

--- js/redlid.js
+++ js/redlid.js
@@ -20,28 +20,7 @@ $('select[id=pickupcycle]').change(function () {
 	
 });
 
-$('select[name=wastetype]').change(function () {
-    if ($(this).val() == 'general') {
-        $('#gen').show();
-    } 
-	else {
-        $('#gen').hide();
-    }
-	
-	if ($(this).val() == 'green') {
-        $('#green').show();
-    } 
-	else {
-        $('#green').hide();
-    }
-	
-	if ($(this).val() == 'hard') {
-        $('#hard').show();
-    } 
-	else {
-        $('#hard').hide();
-    }
-});
+$('select[name=wastetype]').change(function () {    if ($(this).val() == 'general') {        $('.gen').show();    } 	else {        $('.gen').hide();    }		if ($(this).val() == 'green') {        $('.green').show();    } 	else {        $('.green').hide();    }		if ($(this).val() == 'hard') {        $('.hard').show();    } 	else {        $('.hard').hide();    }});
 
 $('select[name=payment]').change(function () {
     if ($(this).val() == 'cc') {
@@ -51,4 +30,4 @@ $('select[name=payment]').change(function () {
         $('#cc').hide();
     }
 	
-});
+});function showzone() {	    if (document.getElementById('zone1selector').checked) {        document.getElementById('zone1').style.display = 'block';    }    else document.getElementById('zone1').style.display = 'none';		    if (document.getElementById('zone2selector').checked) {        document.getElementById('zone2').style.display = 'block';    }    else document.getElementById('zone2').style.display = 'none';		    if (document.getElementById('zone3selector').checked) {        document.getElementById('zone3').style.display = 'block';    }    else document.getElementById('zone3').style.display = 'none';}
