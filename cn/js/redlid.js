d5964b2a4576c7d05c1eadb1234a9c08e62220aa
furiousOyster <tom@dreamcatcher.ai>
2021-06-17 12:41:17 +1200

    format

--- cn/js/redlid.js
+++ cn/js/redlid.js
@@ -1,54 +1,47 @@
-$(document).ready(function() {
-    var the_terms = $("#the-terms");
-
-    the_terms.click(function() {
-        if ($(this).is(":checked")) {
-            $("#submitBtn").removeAttr("disabled");
-        } else {
-            $("#submitBtn").attr("disabled", "disabled");
-        }
-    });
-}); 
-
-$('select[id=pickupcycle]').change(function () {
-    if ($(this).val() == 'eightweekly') {
-        $('#eightweekly').show();
-    } 
-	else {
-        $('#eightweekly').hide();
-    }
-	
-});
-
-$('select[name=wastetype]').change(function () {
-    if ($(this).val() == 'general') {
-        $('#gen').show();
-    } 
-	else {
-        $('#gen').hide();
-    }
-	
-	if ($(this).val() == 'green') {
-        $('#green').show();
-    } 
-	else {
-        $('#green').hide();
-    }
-	
-	if ($(this).val() == 'hard') {
-        $('#hard').show();
-    } 
-	else {
-        $('#hard').hide();
-    }
-});
-
-$('select[name=payment]').change(function () {
-    if ($(this).val() == 'cc') {
-        $('#cc').show();
-    } 
-	else {
-        $('#cc').hide();
-    }
-	
-});
+$(document).ready(function () {
+  var the_terms = $("#the-terms");
+
+  the_terms.click(function () {
+    if ($(this).is(":checked")) {
+      $("#submitBtn").removeAttr("disabled");
+    } else {
+      $("#submitBtn").attr("disabled", "disabled");
+    }
+  });
+});
+
+$("select[id=pickupcycle]").change(function () {
+  if ($(this).val() == "eightweekly") {
+    $("#eightweekly").show();
+  } else {
+    $("#eightweekly").hide();
+  }
+});
+
+$("select[name=wastetype]").change(function () {
+  if ($(this).val() == "general") {
+    $("#gen").show();
+  } else {
+    $("#gen").hide();
+  }
+
+  if ($(this).val() == "green") {
+    $("#green").show();
+  } else {
+    $("#green").hide();
+  }
+
+  if ($(this).val() == "hard") {
+    $("#hard").show();
+  } else {
+    $("#hard").hide();
+  }
+});
+
+$("select[name=payment]").change(function () {
+  if ($(this).val() == "cc") {
+    $("#cc").show();
+  } else {
+    $("#cc").hide();
+  }
+});
