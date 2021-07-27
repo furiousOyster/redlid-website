ad5249ac6e28af70ca1ce9b7966caad5864c9427
Glitch (redlid-website-app) <none>
2021-07-27 22:47:33 +0000

    🎸📻 Checkpoint ./src/app.jsx:32308926/63 ./src/pages/home.jsx:32308926/84:34313641/679 ./src/styles/styles.css:32308926/3639

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../'
 import * as React from "react";
 import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
@@ -5,13 +7,32 @@ import { Link } from "wouter";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-
-
+import Nav from "react-bootstrap/Nav";
 
 export default function Home() {
   return (
     <Container>
       <Row>
+        
+        <Nav
+  activeKey="/home"
+  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
         <Col>test n stuff</Col>
       </Row>
     </Container>
