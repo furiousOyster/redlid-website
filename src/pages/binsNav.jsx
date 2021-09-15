6d569c3b10db15ce4565868d5883f74613460624
Glitch (redlid-website-app) <none>
2021-09-15 05:12:20 +0000

    🍀🐀 Checkpoint ./.glitch-assets:34313641/452 ./src/pages/binsNav.jsx:34313641/5442

--- src/pages/binsNav.jsx
+++ src/pages/binsNav.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 
 export default () => (
   <Navbar
@@ -15,18 +16,21 @@ export default () => (
   >
     <Navbar.Brand href="https://redlid.co.nz">
       <img
-          alt=""
-          src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogo.png?v=1631680153805"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      Reliable Waikato Waste Disposal Services since 1964
+        alt="Reliable Waikato Waste Disposal Services since 1964"
+        src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogoscalable.png?v=1631682207638"
+        height="48"
+        className="d-inline-block align-top"
+      />{" "}
     </Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-        Signed in as: <a href="#login">Mark Otto</a>
-      </Navbar.Text>
+      
+      <Nav className="me-auto">
+        <Nav.Link href="#OrderBins">Order Bins</Nav.Link>
+        <Nav.Link href="#OrderSkips">Order Skips</Nav.Link>
+
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
