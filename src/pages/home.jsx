908ed315aa22f019215d0690089cb84d94a1b5c4
Glitch (redlid-website-app) <none>
2021-09-16 23:01:58 +0000

    🛎😘 Checkpoint ./src/styles/styles.css:34313641/171 ./src/pages/home.jsx:34313641/1429

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,6 +10,7 @@ import Row from "react-bootstrap/Row";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 import MainNav from "./mainNav";
 
 export default function Home() {
@@ -19,14 +20,20 @@ export default function Home() {
         <MainNav />
         <Col className="LandingPagePanel" sm={12} md={6}>
           <a href="#Bins">
+             <h1>Red Lid Garden Bins & Bags</h1>
+            <p>For green and garden waste</p>
             <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fbin.jpg?v=1631831496533" />
-            <h1>Red Lid Garden Bins & Bags</h1>
+           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
+           <Button variant="primary">Browse Bins & Bags</Button>{' '}
           </a>
         </Col>
         <Col className="LandingPagePanel" sm={12} md={6}>
           <a href="#Skips">
-            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fskip.jpg?v=1631831496533" />
             <h1>Red Lid Skips</h1>
+             <p>For green and garden waste</p>
+            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fskip.jpg?v=1631831496533" />
+           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
+           <Button variant="primary">Browse Skips</Button>{' '}
           </a>
         </Col>
       </Row>
