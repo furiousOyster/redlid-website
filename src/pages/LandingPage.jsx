a0c35517b3b22b957f8afa13232476105fc313b4
Glitch (redlid-website-app) <none>
2021-09-19 00:02:29 +0000

    🖼👧 Checkpoint ./src/pages/LandingPage.jsx:34313641/1831 ./src/pages/home.jsx:34313641/16732 ./src/styles/styles.css:34313641/4

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/styles.css";
+import * as React from "react";
+import { animated } from "react-spring";
+import { useWiggle } from "../hooks/wiggle";
+import { Link } from "wouter";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import MainNav from "./mainNav";
+
+export default function Home() {
+  return (
+    <Container fluid id="LandingPage">
+      <Row>
+        <MainNav />
+        <Col className="LandingPagePanel" sm={12} md={6}>
+          <a href="#Bins">
+             <h1>Red Lid Garden Bins & Bags</h1>
+            <p>For green and garden waste</p>
+            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fbin.jpg?v=1631831496533" />
+           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
+           <Button variant="primary">Browse Bins & Bags</Button>{' '}
+          </a>
+        </Col>
+        <Col className="LandingPagePanel" sm={12} md={6}>
+          <a href="#Skips">
+            <h1>Red Lid Skips</h1>
+             <p>For green and garden waste</p>
+            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fskip.jpg?v=1631831496533" />
+           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
+           <Button variant="primary">Browse Skips</Button>{' '}
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,30 +11,25 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
-import MainNav from "./mainNav";
+import BinsNav from "./binsNav";
 
 export default function Home() {
   return (
-    <Container fluid id="LandingPage">
+    <Container fluid>
       <Row>
-        <MainNav />
-        <Col className="LandingPagePanel" sm={12} md={6}>
-          <a href="#Bins">
-             <h1>Red Lid Garden Bins & Bags</h1>
-            <p>For green and garden waste</p>
-            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fbin.jpg?v=1631831496533" />
-           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
-           <Button variant="primary">Browse Bins & Bags</Button>{' '}
-          </a>
-        </Col>
-        <Col className="LandingPagePanel" sm={12} md={6}>
-          <a href="#Skips">
-            <h1>Red Lid Skips</h1>
-             <p>For green and garden waste</p>
-            <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fskip.jpg?v=1631831496533" />
-           <p className="HomepageDescriptionText">Bins and bags for green waste and garden waste, the best prices in the greater Waikato region. We deliver, we collect.</p>
-           <Button variant="primary">Browse Skips</Button>{' '}
-          </a>
+        <BinsNav />
+        <Col>
+          <Container>
+            <Row>
+              <Col>
+                <h1>
+                Red Lid Garden Bags and Bins
+                </h1>
+                
+              
+              </Col>
+            </Row>
+          </Container>
         </Col>
       </Row>
     </Container>
