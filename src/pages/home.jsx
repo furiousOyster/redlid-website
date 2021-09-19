59d50effd195f881d865a758f7cd5f20cb7e9a05
Glitch (redlid-website-app) <none>
2021-09-19 05:29:18 +0000

    ⛄️🐏 Checkpoint ./src/pages/home.jsx:34313641/1432

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -188,49 +188,7 @@ export default function Home() {
                     />
                     I have read and agree to the INSERT BUTTON HERE
                     
-                    
-                    
-    /*                
-                    
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
-
-render(<Example />);
-                    
-                    
-                    
-                    
-                    
-                    */
-                    
-                    
+ 
                     
                     
                     
