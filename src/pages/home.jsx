5c2ed6665bcdd66ebd5b34567fbf415e4c196fff
Glitch (redlid-website-app) <none>
2021-09-20 03:33:56 +0000

    🎙🏓 Checkpoint ./src/pages/home.jsx:32308926/45 ./src/components/router.jsx:32308926/1597 ./src/pages/LandingPage.jsx:32308926/45 ./src/pages/bins.jsx:32308926/45

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/styles.css";
 import * as React from "react";
 import { animated } from "react-spring";
-import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
