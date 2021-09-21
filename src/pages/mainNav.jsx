99c6a2b4e1d9d110016f0aeadd8a5a21df8ebc7b
Glitch (redlid-website-app) <none>
2021-09-21 01:32:49 +0000

    🐾☕️ Checkpoint ./src/pages/mainNav.jsx:32308926/31

--- src/pages/mainNav.jsx
+++ src/pages/mainNav.jsx
@@ -14,7 +14,7 @@ export default () => (
     expand="lg"
     collapseOnSelect
   >
-    <Navbar.Brand href="https://redlid.co.nz">
+    <Navbar.Brand href="/">
       <Image
         alt="Reliable Waikato Waste Disposal Services since 1964"
         src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogoscalable.png?v=1631682207638"
