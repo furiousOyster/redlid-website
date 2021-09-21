8c7b385a40cf94515949d7122539b0714ab052bb
Glitch (redlid-website-app) <none>
2021-09-21 01:21:46 +0000

    🌚💂 Checkpoint ./src/pages/binsNav.jsx:32308926/2363

--- src/pages/binsNav.jsx
+++ src/pages/binsNav.jsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
+import * as React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 
 export default () => (
   <Navbar
@@ -14,13 +14,13 @@ export default () => (
     expand="lg"
     collapseOnSelect
   >
-    <Navbar.Brand href="https://redlid.co.nz">
+    <Navbar.Brand href="/">
       <Image
         alt="Reliable Waikato Waste Disposal Services since 1964"
         src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogoscalable.png?v=1631682207638"
         height="38"
         className="d-inline-block align-top"
-      />{" "}
+      />
     </Navbar.Brand>
 
     <Button variant="outline-secondary" id="ManageServicesBtn">
@@ -36,4 +36,4 @@ export default () => (
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-);
+)
