b90acbd660593feb5370b7a4fd2b4c3a10d9f25c
Glitch (redlid-website-app) <none>
2021-09-21 05:44:43 +0000

    🖇💂 Checkpoint ./src/pages/mainNav.jsx:32308926/2062

--- src/pages/mainNav.jsx
+++ src/pages/mainNav.jsx
@@ -1,9 +1,10 @@
-import * as React from "react";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
+import * as React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'wouter'
 
 export default () => (
   <Navbar
@@ -13,19 +14,19 @@ export default () => (
     expand="lg"
     collapseOnSelect
   >
-    <Navbar.Brand href="/">
-      <Image
-        alt="Reliable Waikato Waste Disposal Services since 1964"
-        src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogoscalable.png?v=1631682207638"
-        height="38"
-        className="d-inline-block align-top"
-      />{" "}
-      <span>Reliable Waikato Waste Disposal Services since 1964</span>
-    </Navbar.Brand>
-
+    <Link href="/">
+      <Navbar.Brand>
+        <Image
+          alt="Reliable Waikato Waste Disposal Services since 1964"
+          src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fredlidlogoscalable.png?v=1631682207638"
+          height="38"
+          className="d-inline-block align-top"
+        />{' '}
+        <span>Reliable Waikato Waste Disposal Services since 1964</span>
+      </Navbar.Brand>
+    </Link>
     <Button variant="outline-secondary" id="ManageServicesBtn">
       Manage Services
     </Button>
-
   </Navbar>
-);
+)
