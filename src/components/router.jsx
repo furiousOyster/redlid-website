236305648a2c8141abec0310215a581d6193d4c6
Glitch (redlid-website-app) <none>
2021-09-21 05:32:45 +0000

    🚤📅 Checkpoint ./src/components/router.jsx:32308926/2

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Switch, Route, Router } from "wouter";
 import About from "../pages/about";
 import LandingPage from "../pages/LandingPage";
-import Bins from '../pages/bins'
+import Bins from '../pages/Bins'
 /**
  * The router is imported in app.jsx
  *
