8a6c9dd8d2d5399bc03bd1412d422d8556858494
Glitch (redlid-website-app) <none>
2021-09-22 05:11:33 +0000

    🎓💓 Checkpoint ./src/pages/Bins.jsx:34313641/30118

--- src/pages/Bins.jsx
+++ src/pages/Bins.jsx
@@ -60,8 +60,7 @@ export default function Bins() {
             </Row>
           </a>
         </Row>
-        
-        
+
         {/* LISTING BAGS */}
         <Row>
           <a className="Product">
@@ -216,8 +215,7 @@ export default function Bins() {
             </button>
           </form>
         </Row>
-        
-        
+
         {/* HOW IT WORKS */}
 
         <Row>
@@ -232,15 +230,171 @@ export default function Bins() {
             </p>
           </Col>
           <Col>
-          
-            <i>
-            </i><p>There are no contracts to sign</p>
+            <i></i>
+  <p><strong>THERE ARE NO CONTRACTS TO SIGN</strong></p>
 
-  <p>Guaranteed exact date of collection</p>
+  <p><strong>GUARANTEED  EXACT DAY OF COLLECTION</strong></p>
           </Col>
         </Row>
-        
-  
+        <row>
+          <table border="0" cellspacing="0" cellpadding="0" width="0">
+            <tr>
+              <td width="25%" valign="top">
+                Weekly{' '}
+              </td>
+
+              <td width="74%" valign="top">
+                <p>Hamilton only</p>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="25%" valign="top">
+                <p>2 Weekly</p>
+              </td>
+
+              <td width="74%" valign="top">
+                <p>
+                  Hamilton, Te Awamutu, Cambridge, Morrinsville, Matamata,
+                  Ngaruawahia, Te Kowhai
+                </p>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="25%" valign="top">
+                <p>4 Weekly</p>
+              </td>
+
+              <td width="74%" valign="top">
+                <p>
+                  Hamilton, Te Awamutu, Cambridge, Morrinsville, Matamata,
+                  Ngaruawahia, Te Kowhai
+                </p>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="25%" valign="top">
+                <p>8 Weekly</p>
+              </td>
+
+              <td width="74%" valign="top">
+                <p>
+                  Hamilton, Te Awamutu, Cambridge, Morrinsville, Matamata,
+                  Ngaruawahia, Te Kowhai
+                </p>
+              </td>
+            </tr>
+          </table>
+
+          <p>
+            These are some of the benefits of why Red Lid Garden Bins &amp; Bags
+            may work for you
+          </p>
+
+          <ul>
+            <li>
+              We are in every suburb every week on the <strong>SAME DAY</strong>{' '}
+              (Hamilton only)
+            </li>
+
+            <li>Extra collections are no problem</li>
+
+            <li>We can also leave extra bags for big clean up&rsquo;s</li>
+
+            <li>
+              We can do one off/casual collections of a 600 litre Garden Bag
+            </li>
+
+            <li>
+              If you have your own bin or bag we can also do a one off
+              collection
+            </li>
+          </ul>
+
+          <p>
+            Garden Bags can be located anywhere on your property with easy
+            access- we collect off your section with a hand trolley.
+          </p>
+
+          <p>
+            {' '}
+            Garden Bins are collected from the kerbside on your collection day. 
+            Please have the Bin on the kerb by 7am.  We service each suburb on
+            the same day each week.  If for some reason you cannot get your
+            Garden Bin to the kerb you can arrange with us to do so at{' '}
+            <strong>NO EXTRA COST</strong>.
+          </p>
+
+          <p>
+            <strong>
+              &ldquo;WE BELIEVE WE WILL NOT BE BEATEN ON SERVICE&rdquo;
+            </strong>
+          </p>
+
+          <p>
+            <strong>
+              OUR VISION IS TO BE THE NUMBER 1 GREEN WASTE COLLECTOR IN THE
+              WAIKATO
+            </strong>
+          </p>
+
+          <h2>About us</h2>
+
+          <p>
+            We are proud to be rated the Number 1 Green Waste Collectors in the
+            Waikato.
+          </p>
+
+          <p>
+            Hamilton Garden Bags was established in 1989 and then purchased by
+            brothers Grant &amp; Steve Wilson in 1998.&nbsp; In 2015 the name
+            was changed to Red Lid Garden Bins &amp; Bags Ltd.&nbsp; We started
+            out with one tandem trailer which was very hard work in the early
+            days.&nbsp; But through hard work and a commitment to providing the
+            best customer service, we are now proud of our fleet of 7 purpose
+            built compactor trucks.{' '}
+          </p>
+
+          <p>
+            {' '}
+            Our Vision to be The Number 1 Green Waste Collector in the Waikato
+            drives our passion to deliver the best possible customer
+            service.&nbsp; We have a team that believes in our Vision and we are
+            proud of what we achieve every day.
+          </p>
+
+          <p>
+            We provide more Collection Frequency options than any other company
+            in the Waikato.&nbsp; “Good as Gold”&nbsp; “Not a problem” is all
+            part of our very prompt and reliable service. We pride ourselves on
+            delivering the best service in the Waikato.
+          </p>
+
+          <p>
+            {' '}
+            We are proud to be doing our part for the environment as all the
+            Greenwaste we collect is recycled into compost products which
+            reduces the estimated 40% of Greenwaste that goes to landfills each
+            year.
+          </p>
+
+          <p>
+            2018 has been an exciting year with the addition of RedLid Skips.
+            This came about as our existing customers were contacting us on a
+            regular basis and asking if we had skips. The decision was made to
+            provide skips but not just for Greenwaste. We also provide skips for
+            General waste and Hard fill. At this stage we are only providing
+            skips in Hamilton and Cambridge.
+          </p>
+
+          <p>
+            Red Lid Garden Bins &amp; Bags are proud to be 100% Hamilton owned
+            &amp; operated and committed to providing the best Green Waste
+            Collection service in the Waikato.{' '}
+          </p>
+        </row>
       </Container>
       <ModalTerms show={showTerms} onHide={() => setShowTerms(false)} />
       <ModalFaqs show={showFaqs} onHide={() => setShowFaqs(false)} />
