eeeb8e34f7f32ad73458d7baec82330d3db5c222
Glitch (redlid-website-app) <none>
2021-09-27 23:09:53 +0000

    💃🍙 Checkpoint ./src/components/Footer.jsx:34313641/5288 ./src/pages/LandingPage.jsx:34313641/2206 ./src/pages/Bins.jsx:34313641/1832 ./src/pages/Skips.jsx:34313641/2195

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,6 +11,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import MainNav from '../components/MainNav'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -49,82 +50,7 @@ export default function Home() {
         </Row>
       </Container>
       
-        {/* FOOTER */}
-      <Container fluid className="Footer">
-        <Row>
-          <h2>We are easy to contact</h2>
-          <Col md={6}>
-            <p>
-              Red Lid Garden Bins &amp; Bags Ltd
-              <br />
-              P.O. Box 24087
-              <br />
-              Hillcrest
-              <br />
-              Hamilton 3253
-            </p>
-          </Col>
-          <Col md={6}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Phone</td>
-
-                  <td>
-                    <Link href="tel:078340191">(07)&nbsp;834&nbsp;0191</Link>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td></td>
-
-                  <td>
-                    <Link href="tel:0800733543">
-                      0800 REDLID (0800&nbsp;733543)
-                    </Link>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td>Email</td>
-
-                  <td>
-                    <Link href="mailto:service@redlid.co.nz">
-                      service@redlid.co.nz
-                    </Link>
-                  </td>
-                </tr>
-
-                <tr>
-                  <td></td>
-
-                  <td>
-                    <Link href="/manage">Manage my services</Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </Col>
-        </Row>
-        <Row>
-          <p>
-            Please feel free to contact us about anything you may require or
-            need further explanation on.
-          </p>
-
-          <p class="officehours">
-            Office hours are 8am - 5pm Monday to Thursday, 8am-3pm Fridays
-          </p>
-
-          <p>
-            If outside these hours leave a message on{' '}
-            <Link href="tel:078340191">(07) 834 0191</Link> or{' '}
-            <Link href="tel:0800733543">0800 REDLID</Link> answer phone - we
-            check our messages regularly. We look forward to providing you with
-            prompt, friendly and professional service.
-          </p>
-        </Row>
-      </Container>
+    <Footer />
 
     </>
     
