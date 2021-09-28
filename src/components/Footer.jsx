e1633a6aac8cec7d4bd383d07f6c2328381b69af
Glitch (redlid-website-app) <none>
2021-09-28 22:09:08 +0000

    🌲🌚 Checkpoint ./src/components/Footer.jsx:34313641/5240 ./src/styles/styles.css:34313641/880

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,83 +9,81 @@ import Row from 'react-bootstrap/Row'
 import { Link } from 'wouter'
 
 export default () => (
-      <Container fluid className="Footer">
-      <Container fluid className="FooterTruck">
-     <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidTruckFooter.jpg?v=1632865751569" />
-        
-          <h2>We are easy to contact</h2>
-          <Col md={6}>
-            <p>
-              Red Lid Garden Bins &amp; Bags Ltd
-              <br />
-              P.O. Box 24087
-              <br />
-              Hillcrest
-              <br />
-              Hamilton 3253
-            </p>
-          </Col>
-          <Col md={6}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Phone</td>
+  <Container fluid className="Footer">
+    <Row className="FooterTruck">
+      <Col className="FooterHeading">
+        <h2>We are easy to contact</h2>
+      </Col>
+      <Col md={6}>
+        <p>
+          Red Lid Garden Bins &amp; Bags Ltd
+          <br />
+          P.O. Box 24087
+          <br />
+          Hillcrest
+          <br />
+          Hamilton 3253
+        </p>
+      </Col>
+      <Col md={6}>
+        <table>
+          <tbody>
+            <tr>
+              <td>Phone</td>
 
-                  <td>
-                    <Link href="tel:078340191">(07)&nbsp;834&nbsp;0191</Link>
-                  </td>
-                </tr>
+              <td>
+                <Link href="tel:078340191">(07)&nbsp;834&nbsp;0191</Link>
+              </td>
+            </tr>
 
-                <tr>
-                  <td></td>
+            <tr>
+              <td></td>
 
-                  <td>
-                    <Link href="tel:0800733543">
-                      0800 REDLID (0800&nbsp;733543)
-                    </Link>
-                  </td>
-                </tr>
+              <td>
+                <Link href="tel:0800733543">
+                  0800 REDLID (0800&nbsp;733543)
+                </Link>
+              </td>
+            </tr>
 
-                <tr>
-                  <td>Email</td>
+            <tr>
+              <td>Email</td>
 
-                  <td>
-                    <Link href="mailto:service@redlid.co.nz">
-                      service@redlid.co.nz
-                    </Link>
-                  </td>
-                </tr>
+              <td>
+                <Link href="mailto:service@redlid.co.nz">
+                  service@redlid.co.nz
+                </Link>
+              </td>
+            </tr>
 
-                <tr>
-                  <td></td>
+            <tr>
+              <td></td>
 
-                  <td>
-                    <Link href="/manage">Manage my services</Link>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </Col>
-        </Row>
-        <Row>
-          <p>
-            Please feel free to contact us about anything you may require or
-            need further explanation on.
-          </p>
+              <td>
+                <Link href="/manage">Manage my services</Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Col>
+      <Col>
+        <p>
+          Please feel free to contact us about anything you may require or need
+          further explanation on.
+        </p>
 
-          <p class="officehours">
-            Office hours are 8am - 5pm Monday to Thursday, 8am-3pm Fridays
-          </p>
+        <p className="officehours">
+          Office hours are 8am - 5pm Monday to Thursday, 8am-3pm Fridays
+        </p>
 
-          <p>
-            If outside these hours leave a message on{' '}
-            <Link href="tel:078340191">(07) 834 0191</Link> or{' '}
-            <Link href="tel:0800733543">0800 REDLID</Link> answer phone - we
-            check our messages regularly. We look forward to providing you with
-            prompt, friendly and professional service.
-          </p>
-        </Row>
-     </Container>
-      </Container>
-
+        <p>
+          If outside these hours leave a message on{' '}
+          <Link href="tel:078340191">(07) 834 0191</Link> or{' '}
+          <Link href="tel:0800733543">0800 REDLID</Link> answer phone - we check
+          our messages regularly. We look forward to providing you with prompt,
+          friendly and professional service.
+        </p>
+      </Col>
+    </Row>
+  </Container>
 )
