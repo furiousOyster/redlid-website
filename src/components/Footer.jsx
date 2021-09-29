510c31754274c48211554114c9e06f6262de167f
Glitch (redlid-website-app) <none>
2021-09-29 00:16:12 +0000

    🚦📮 Checkpoint ./src/styles/styles.css:34313641/283 ./src/components/Footer.jsx:34313641/1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ import { Link } from 'wouter'
 
 export default () => (
   <Container fluid className="Footer">
-    <Row className="FooterTruck">
+    <Row className="FooterTruckx">
       <Col className="FooterHeading">
         <h2>We are easy to contact</h2>
       </Col>
