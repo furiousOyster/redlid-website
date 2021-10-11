2b2f73e8933ac505494f88196db9cc4f53492f2f
Glitch (redlid-website-app) <none>
2021-10-11 02:13:37 +0000

    😽🔖 Checkpoint ./src/styles/styles.css:34313641/300 ./src/app.jsx:34313641/339

--- src/app.jsx
+++ src/app.jsx
@@ -26,21 +26,12 @@ export default function Home() {
   return (
     <Router hook={useHashLocation}>
       <Seo />
-      <main role="main" className="wrapper">
+      <main role="main" className="wrapper" id="Redlid">
         <div className="content">
           {/* Router specifies which component to insert here as the main content */}
           <PageRouter />
         </div>
       </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx 
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-      </footer>
-      */}
     </Router>
   );
 }
