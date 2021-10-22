240363aadd42ad4843240f810acbcb226d46e9e1
Glitch (redlid-website-app) <none>
2021-10-22 20:55:09 +0000

    🔋😹 Checkpoint ./.glitch-assets:34313641/425 ./src/components/BinsNav.jsx:34313641/192 ./src/components/SkipsNav.jsx:34313641/192 ./src/components/NavbarBrand.jsx:34313641/192 ./src/styles/styles.css:34313641/17110

--- src/components/NavbarBrand.jsx
+++ src/components/NavbarBrand.jsx
@@ -7,7 +7,7 @@ export default () => (
     <Navbar.Brand href="/">
       <Image
         alt="Red Lid Garden Bins and Bags in Waikato, New Zealand."
-        src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidLogo40pxH.png?v=1634935238604"
+        src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidLogo40pxH.png?v=1634935640796"
         height="40"
         className="d-inline-block align-top"
       />
