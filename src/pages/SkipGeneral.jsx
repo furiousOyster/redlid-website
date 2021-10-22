1f16d380ae14e154d6ba516c853fca1280186ce4
Glitch (redlid-website-app) <none>
2021-10-22 21:40:02 +0000

    🌑🛋 Checkpoint ./src/styles/styles.css:34313641/30 ./src/pages/SkipGeneral.jsx:34313641/684

--- src/pages/SkipGeneral.jsx
+++ src/pages/SkipGeneral.jsx
@@ -127,6 +127,43 @@ export default function Skips() {
                 We'll never share your email with anyone else.
               </small>
             </FloatingLabel>
+            
+            
+            
+            
+              {['checkbox', 'radio'].map((type) => (
+    <div key={`inline-${type}`} className="mb-3">
+      <Form.Check
+        inline
+        label="1"
+        name="group1"
+        type={type}
+        id={`inline-${type}-1`}
+      />
+      <Form.Check
+        inline
+        label="2"
+        name="group1"
+        type={type}
+        id={`inline-${type}-2`}
+      />
+      <Form.Check
+        inline
+        disabled
+        label="3 (disabled)"
+        type={type}
+        id={`inline-${type}-3`}
+      />
+    </div>
+  ))}
+            
+            
+            
+            
+            
+            
+            
+            
 
             <FloatingLabel label="Skip size">
               {' '}
