46b4f69302188e69a1c176ecae0f86e9592ce163
Glitch (redlid-website-app) <none>
2021-11-01 18:07:30 +0000

    😼🏓 Checkpoint ./src/pages/BinsBags.jsx:34313641/7424

--- src/pages/BinsBags.jsx
+++ src/pages/BinsBags.jsx
@@ -17,6 +17,7 @@ import ModalBody from 'react-bootstrap/ModalBody'
 import ModalFooter from 'react-bootstrap/ModalFooter'
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 import ModalFaqs from '../components/ModalFaqs'
 import ModalTerms from '../components/ModalTerms'
@@ -114,28 +115,32 @@ export default function BinsBags() {
           <h2>Blog post example</h2>
           <i>24 September 2021</i>
           <Col>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/tL6SSflpnkY"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              fermentum, nulla luctus pharetra vulputate, felis tellus mollis
-              orci, sed rhoncus pronin sapien nunc accuan eget.
-            </p>
 
             <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2Fbin.jpg?v=1631831496533" />
 
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-              fermentum, nulla luctus pharetra vulputate, felis tellus mollis
-              orci, sed rhoncus pronin sapien nunc accuan eget.
+             Summer is here and it's time to take care of that spring overgrowth! Our job is to make your garden waste removal as simple and cheap as possible.
+              So here are our top eight tips for the most efficient rubbish skip fillery imagineable!
             </p>
+            <ListGroup as="ol" numbered>
+  <ListGroup.Item as="li">Crasjustoodio</ListGroup.Item>
+</ListGroup>
+            1. 
+
+2. Look closely at our Redlid skip hire pictures on our website. This is the best way to ensure you are ordering the correct size Redlid skip. Note all the related skip hire prices are recorded on our site.
+
+3. Check the weather forecast as loading a skip in the rain is not much fun and can add
+to the cost.
+
+4. If you want to make an early morning start loading your Redlid skip, order it for the afternoon before you need it. Skip companies often have planned drop off schedules which means that you may not get your bin in time to start loading at that exact time.
+
+5. If it rains, put a tarpaulin over the skip to cover it and keep water out. You don’t want to be paying extra over-weight charges for a heavy wet load as transfer stations charge only by weight.
+
+6. If you are unsure of Skip size, it pays to advise us and go for the next size up. For example, to help you do this, we will supply a 9m bin when you order a 6m just in case you need to fill the extra capacity. This would save time, and it would cost a lot more money if you had to order a second bin.
+
+7. And always be careful when loading a skip. Accidents can occur if you try to climb the side of the skip or entering the skip.
+
+8. If you are unsure about what goes into a Redlid skip check the terms and conditions or contact one of our friendly office staff. You don’t want extra charges because you have put the wrong items in a skip.
           </Col>
         </Row>
 
