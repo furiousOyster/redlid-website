9bdcfb608b5fc9c9068b3a592ceec1b9815c9751
Glitch (redlid-website-app) <none>
2021-11-01 17:43:28 +0000

    📡🚠 Checkpoint ./src/pages/Bins.jsx:34313641/32943

--- src/pages/Bins.jsx
+++ src/pages/Bins.jsx
@@ -23,8 +23,6 @@ import ModalTerms from '../components/ModalTerms'
 import BinsNav from '../components/BinsNav'
 import Footer from '../components/Footer'
 
-
-
 export default function Bins() {
   const [showTerms, setShowTerms] = useState(false)
   const [showFaqs, setShowFaqs] = useState(false)
@@ -43,10 +41,9 @@ export default function Bins() {
           <h1>Order a 240L Wheelie Bin</h1>
         </Row>
       </Container>
-
       <Container id="pdp">
         <Row className="pdptop">
-          <Image src="https://cdn.glitch.com/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlid_Greenwaste_Skip.jpg?v=1632527973536" />
+          <Image src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidBin.jpg?v=1634953281368" />
           <Link href="/Bags">
             <Button variant="outline-primary" className="ProductSwap">
               Order a 600L bag instead
@@ -57,38 +54,94 @@ export default function Bins() {
         {/* ORDER FORM */}
 
         <Row>
+          <i className="pdpHelptext">
+            <p>
+              Clean and convenient – wheel it to where you are working in the
+              garden.
+            </p>
+            <p>
+              Please have your bin on the kerbside by 7am on your collection day
+              – same weekday every collection.
+            </p>
+          </i>
+
           <Form>
             <FloatingLabel
               controlId="floatingInput"
-              label="Find your address or postcode"
+              label="Find your address"
               className="mb-3"
             >
               <Form.Control type="text" placeholder="Enter your address" />
             </FloatingLabel>
+            
+            
+             <h2 className="blacklabel">Choose pickup cycle</h2>
+
+            <Form.Check
+              label="Weekly (Hamilton only) - $8 per collection"
+              name="PickupCycle"
+              type="inline-radio"
+              id="InternetBanking"
+            />
+
+                 <Form.Check
+              label="Internetbadbvasdefsdfasdfas"
+              name="PickupCycle"
+              type="inline-radio"
+              id="InternetBanking"
+            />
+                 <Form.Check
+              label="Internetbadbvasdefsdfasdfas"
+              name="PickupCycle"
+              type="inline-radio"
+              id="InternetBanking"
+            />
+                 <Form.Check
+              label="Internetbadbvasdefsdfasdfas"
+              name="PickupCycle"
+              type="inline-radio"
+              id="InternetBanking"
+            />
+            
+            
+            
+            
+            
+            
+            
+
             <FloatingLabel
+              id="fullName"
               controlId="floatingInput"
               label="Enter your full name"
               className="mb-3"
             >
               <Form.Control type="text" placeholder="Your name" />
             </FloatingLabel>
+
             <FloatingLabel
+              id="phone"
               controlId="floatingInput"
               label="Phone number"
               className="mb-3"
             >
               <Form.Control type="phone" placeholder="Phone number" />
             </FloatingLabel>
+
             <FloatingLabel
+              id="email"
               controlId="floatingInput"
               label="Email address"
               className="mb-3"
+              aria-describedby="emailHelp"
             >
               <Form.Control type="email" placeholder="name@example.com" />
-              <small id="emailHelp" class="form-text text-muted">
+
+              <Form.Text id="emailHelp" muted>
                 We'll never share your email with anyone else.
-              </small>
+              </Form.Text>
             </FloatingLabel>
+
             <FloatingLabel label="Choose pickup cycle">
               <Form.Select aria-label="Floating label select example">
                 <option>Weekly - $18 per collection</option>
@@ -99,15 +152,27 @@ export default function Bins() {
                 </option>
               </Form.Select>
             </FloatingLabel>
-            <FloatingLabel label="Payment method">
-              <Form.Select aria-label="Floating label select example">
-                <option>Internet banking</option>
-                <option value="cc">Credit card (+3% surcharge)</option>
-              </Form.Select>
-              <small class="form-text text-muted" id="cc">
-                We will contact you by phone for your credit card details.
-              </small>
-            </FloatingLabel>
+
+            <h2 className="blacklabel">Payment method</h2>
+
+            <Form.Check
+              label="Internet banking"
+              name="PaymentMethod"
+              type="inline-radio"
+              id="InternetBanking"
+            />
+
+            <Form.Check
+              label="Credit card"
+              name="PaymentMethod"
+              type="inline-radio"
+              id="CreditCard"
+            />
+
+            <Form.Text id="WeightHelp" muted>
+              We will contact you by phone for your credit card details.
+            </Form.Text>
+
             <FloatingLabel controlId="floatingTextarea2" label="Comments">
               <Form.Control
                 as="textarea"
@@ -142,12 +207,9 @@ export default function Bins() {
             </button>
           </Form>
         </Row>
-      
       </Container>{' '}
       {/* END OF CONTENT CONTAINER */}
-     
-    <Footer />
-      
+      <Footer />
       <ModalTerms show={showTerms} onHide={() => setShowTerms(false)} />
       <ModalFaqs show={showFaqs} onHide={() => setShowFaqs(false)} />
     </>
