829924e8858306f2d818bf7565fbd50647647817
Glitch (redlid-website-app) <none>
2021-11-10 22:12:46 +0000

    🏟🍯 Checkpoint ./.glitch-assets:34313641/1115 ./src/pages/SkipGeneral.jsx:34313641/534 ./src/pages/SkipGreen.jsx:34313641/882 ./src/pages/Bins.jsx:34313641/899

--- src/pages/Bins.jsx
+++ src/pages/Bins.jsx
@@ -23,6 +23,16 @@ import ModalTerms from '../components/ModalTerms'
 import BinsNav from '../components/BinsNav'
 import Footer from '../components/BinsFooter'
 
+
+
+
+
+
+
+
+
+
+
 export default function Bins() {
   const [showTerms, setShowTerms] = useState(false)
   const [showFaqs, setShowFaqs] = useState(false)
@@ -43,7 +53,7 @@ export default function Bins() {
       </Container>
       <Container id="pdp">
         <Row className="pdptop">
-       
+          <Image src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidBin.jpg?v=1634953281368" />
         </Row>
 
         {/* ORDER FORM */}
--- src/pages/SkipGeneral.jsx
+++ src/pages/SkipGeneral.jsx
@@ -123,7 +123,7 @@ export default function Skips() {
                 <Col xs={12} sm={4}>
                   <Image
                     fluid
-                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FSKipSize9m.jpg?v=1634945630545"
+                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2F9mSKip.png?v=1636582031070"
                   />
                   <Form.Check
                     label="9m² ($350)"
@@ -137,7 +137,7 @@ export default function Skips() {
                 <Col xs={12} sm={4}>
                   <Image
                     fluid
-                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FSKipSize6m.jpg?v=1634945630563"
+                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2F6mSKip.png?v=1636582031070"
                   />
                   <Form.Check
                     label="6m² ($290)"
@@ -151,7 +151,7 @@ export default function Skips() {
                 <Col xs={12} sm={4}>
                   <Image
                     fluid
-                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FSKipSize3m.jpg?v=1634945630510"
+                    src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2F3mSKip.png?v=1636582031070"
                   />
                   <Form.Check
                     label="3m² ($240)"
