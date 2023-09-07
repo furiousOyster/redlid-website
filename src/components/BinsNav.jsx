5dfb836af727923923d1db84343afc3d31ced29f
Glitch (redlid-website-app) <none>
2023-09-07 03:53:13 +0000

    ⚽️🚔 Checkpoint ./src/components/BinsNav.jsx:61155602/2910

--- src/components/BinsNav.jsx
+++ src/components/BinsNav.jsx
@@ -6,45 +6,30 @@ import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'wouter'
 
-   
 export default () => (
-  <Navbar
-    bg="#fff"
-    variant="light"
-    expand="lg"
-    collapseOnSelect
-    id="BinsNav"
-  >
-    <Navbar.Brand href="/">
-      <Image
-        alt="Red Lid Garden Bins and Bags in Waikato, New Zealand."
-        src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidLogo40pxH.png?v=1634935640796"
-        height="40"
-        className="d-inline-block align-top"
-      />
-      <h1>Garden bins & bags</h1>
-    </Navbar.Brand>
+  <Navbar bg="#fff" variant="light" expand="lg" collapseOnSelect id="BinsNav">
+    <Container>
+      <Navbar.Brand href="/">
+        <Image
+          alt="Red Lid Garden Bins and Bags in Waikato, New Zealand."
+          src="https://cdn.glitch.me/62dc5d7b-74d0-43a4-84cf-9d776e17c335%2FRedlidLogo40pxH.png?v=1634935640796"
+          height="40"
+          className="d-inline-block align-top"
+        />
+        <h1>Garden bins & bags</h1>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="me-auto CollapsableNav">
+          <Nav.Link href="#HowItWorks">How it works</Nav.Link>
+          
+          <Nav.Link className="FAQBtn">
+            FAQs
+          </Nav.Link>
 
-    <Link href="/Manage">
-      <Button variant="outline-secondary" id="ManageServicesBtn">
-        Manage Services
-      </Button>
-    </Link>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Nav className="me-auto CollapsableNav">
-        <Link href="#HowItWorks">How it works</Link>
-        
-        <Button
-                className="FAQBtn"
-                variant="link"
-              >
-                FAQs
-              </Button>
-        
-        <Link href="#Contact">Contact</Link>
-      </Nav>
-    </Navbar.Collapse>
+          <Nav.Link href="#Contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
-      
-  )
+)
