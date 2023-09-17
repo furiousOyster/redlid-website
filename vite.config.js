7410bd4ff38c86314710b1973c5072a358b9d507
inverted_capital <evil_liskov@protonmail.com>
2023-09-18 10:12:57 +1200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react'
 
+const isGlitchEnvironment = !!process.env.GLITCH_SHARED_INCLUDES_LEGACY_CLS
+
+const glitch = {
+  host: '0.0.0.0',
+  port: 3000,
+  strictPort: true,
+  hmr: {
+    clientPort: 443, // Run the websocket server on the SSL port
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     outDir: 'build',
   },
-  server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  },
+  server: isGlitchEnvironment ? glitch : {},
 })
